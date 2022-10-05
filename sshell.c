fe7197ae917eb49f82de275ca59b2871c33568bd
Jillian Lim <36285324+jillian12@users.noreply.github.com>
2022-10-05 01:33:22 -0700

    while loop in parse args
    fixed while loop in parse arguments by making it go in createProcess correctly (i think)

--- sshell.c
+++ sshell.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 #define CMDLINE_MAX 512
  
-
 //cd /home/cs150jp/public/p1/ 
-
 
 // making a struct which will make an array of string args to char args*[] for execvp
 // struct cmdArgs{
@@ -18,7 +17,6 @@
 
 int argHandler(char* arguments[], char []); 
 void createProcess(char *[]);
-
 
 void createProcess(char *cleanedArguments[]){
                 pid_t pid;
@@ -89,103 +87,86 @@ void createProcessWithOutputRedirection(char *cleanedArguments[], char* outputFi
                         cleanedArguments[0], WEXITSTATUS(status)); //change to full command string later
 }
 
+
 int argHandler(char* arguments[], char workdir[]) {
-        int currPos, toFileFD;    
-        //char workdir[512];
-        char *cleanedArguments[16]; 
-        char *n1;
-        
-        //n2 = strchr(cmd, '\n');
-        //n3 = strchr(cmd, '\n');    
-        // > 
-        // < 
-        // | 
-        
-        //echo hello world > test | cat test 
-        // echo echo test | 11
-        
-        while(arguments[currPos] != NULL){
-               n1 = strchr(arguments[currPos], '>');
-               //if not < > >&  | add to cleanargs 
-               //else deal with symbol
-               //char *output = strchr(cmd, '>');
+    int currPos = 0;
+    //char workdir[512];
+    char *cleanedArguments[16];
+    char *outRedirect, *pipe, *inRedirect;
+    bool hasChar = false;
 
-               // If not a > < or | then go in if statement  
-               if((n1 == 0) ){
-                //    || (strcmp(arguments[currPos], "<") != 0)
-                //    || (strcmp(arguments[currPos], "|") != 0)) 
-                        cleanedArguments[currPos] = arguments[currPos];
-                        printf("args: %s\t%d\n", cleanedArguments[currPos], currPos);
-                        currPos++;
-                        }
-               else{   
-                        if(n1 != 0) {
-                                //  toFileFD = open(arguments[currPos++], O_WRONLY | O_CREAT, 0644);
-                                //  savedSTDO = dup(STDOUT_FILENO);
-                                //  dup2(toFileFD, STDOUT_FILENO);
-                                //  close(toFileFD);
-                                //  currPos++;
+    //n2 = strchr(cmd, '\n');
+    //n3 = strchr(cmd, '\n');
+    // >
+    // <
+    // |
 
+    //echo hello world > test | cat test
+    // echo echo test | 11
 
-                                // dup2(savedSTDO, toFileFD);
-                                // close(savedSTDO);
-                                printf("In statement\n");
-                                cleanedArguments[currPos] = NULL;
-                                createProcessWithOutputRedirection(cleanedArguments, arguments[currPos+1]);
-                                currPos += 2;
-                                break; // this will then break out of the else and continue going through argumetns
-                        
-                         } else if(strcmp(arguments[currPos], "<") == 0) {
-                                //then handle <
-                                break;
-                         }
-                         else{
-                                cleanedArguments[currPos] = NULL;
-                                createProcess(cleanedArguments);
-                         }
-                         
-
-                        break;
-               }
-               // check for pipe 
-               //checkpipe() which will be like below
-               // can be up to 3 pipe signs on the same command line 
-               /*
-                if  (strcmp(arguments[currPos], "|") != 0) {
-                        // process 1 | process 2 | process 3
-                        
-                }
-
-
-               */
+    while(arguments[currPos] != NULL){
+        // initialize special characters
+        outRedirect = strchr(arguments[currPos], '>');
+        pipe = strchr(arguments[currPos], '|');
+        inRedirect = strchr(arguments[currPos], '<');
+        //if not < > >&  | add to cleanargs
+        // If not a > < or | then go in if statement
+        if(outRedirect == 0 && pipe == 0 && inRedirect == 0){
+            cleanedArguments[currPos] = arguments[currPos];
+            printf("args: %s\t%d\n", cleanedArguments[currPos], currPos);
+            currPos++;
         }
-     
-        // /* Builtin command */
-        // //This will be the exit command 
-        if (!strcmp(cleanedArguments[0], "exit")) {
-                fprintf(stderr, "Bye...\n");
-                exit(0);
-        } 
-        // the is the command for pwd 
-        else if (!strcmp(cleanedArguments[0], "pwd")) {
-                printf("CURRENT DIR: %s\n", getcwd(workdir, CMDLINE_MAX));
-        } 
-        else if (!strcmp(cleanedArguments[0], "cd")) {
-               chdir(cleanedArguments[1]); 
-        } 
-        // else if(strcmp(arguments[currPos], ">") == 0){
-                
-        // }
         else{
+            if(outRedirect != 0) {
+                // this will handle the > output redirection
+                printf("In carrot statement\n");
+                hasChar = true;
+                cleanedArguments[currPos] = NULL;
+                createProcessWithOutputRedirection(cleanedArguments, arguments[currPos+1]);
+                currPos += 2;
+                break; // this will then break out of the else and continue going through argumetns
 
+            } else if(inRedirect != 0) {
+                printf("In < Statement\n");
+                //then handle input redirection <
+                break;
+            }
+//            else{
+//                cleanedArguments[currPos] = NULL;
+//                createProcess(cleanedArguments);
+//            }
+            break;
         }
+        // check for pipe
+        //checkpipe() which will be like below
+        // can be up to 3 pipe signs on the same command line
+         if  (pipe != 0) {
+                 // process 1 | process 2 | process 3
+                 // handle pipe after checking if no > or <
+                 printf("In | Statement\n");
+         }
+    }
 
-        // cleanedArguments[currPos] = NULL;
-        // createProcess(cleanedArguments);
-        //if > handleioredirection(cleanargs, fd)
-     
-        return 0;
-
+    // /* Builtin commands */
+    // //This will be the exit command
+    if (!strcmp(cleanedArguments[0], "exit")) {
+        fprintf(stderr, "Bye...\n");
+        exit(0);
+    }
+    // the is the command for pwd
+    else if (!strcmp(cleanedArguments[0], "pwd")) {
+        printf("CURRENT DIR: %s\n", getcwd(workdir, CMDLINE_MAX));
+    }
+    // this is the command for cd
+    else if (!strcmp(cleanedArguments[0], "cd")) {
+        chdir(cleanedArguments[1]);
+    }
+    // this will create a process if string doesnt have a > < or |
+    else if (!hasChar) {
+        cleanedArguments[currPos] = NULL;
+        createProcess(cleanedArguments);
+    }
+    return 0;
 }
 
 
@@ -226,17 +207,10 @@ int main(void)
                 //         *n2 = ' ';
                 //         toFileBool = 1;
                 // }
-
-                //printf("cmd raw: %s\n",cmd);
-
-               
-
-
                 //raw tokens
                 //add initial token
                 char *token = strtok(cmd, " ");
                 arguments[0] = token;
-
 
                 while(token != NULL) {
                         //make object 
@@ -247,7 +221,6 @@ int main(void)
                        // printf("token2: %s\t%d\n",token, inc);
                 }
 
-        
                 argHandler(arguments, workdir);
 
                 // pid_t pid;
